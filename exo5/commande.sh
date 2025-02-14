05712cb5b20e36638bb40d606e455671fa304705
ymachi <amydiawara75@gmail.com>
2025-02-14 16:26:35 +0100

    exo5

---
+++ exo5/commande.sh
@@ -0,0 +1,3 @@
+docker build -t my-exo5 .
+# ça ne fonctionne par car le terminal n'arrive pas à lire le dockerfile, j'ai pourtant fait la commande cat et il le récupère bien.
+ # c'est bon je n'avais pas pas mis mon app.js dans le CMD 
